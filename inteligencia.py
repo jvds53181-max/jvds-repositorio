66fe21acf11e6f8e58a4880be787515773a78de2
jvds53181-max <jvds53181@gmail.com>
2026-03-08 20:38:32 -0300

    Update greeting message from Marlene to Jorge

--- inteligencia.py
+++ inteligencia.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
-st.write('# escreve algo aí Marlene Silva')
+st.write('# escreve algo aí Jorge')
 mensagem = st.chat_input("Escreva sua mensagem aqui")
 
 st.chat_message('user').write(mensagem)
 
 print(mensagem)
+
 
 
 
